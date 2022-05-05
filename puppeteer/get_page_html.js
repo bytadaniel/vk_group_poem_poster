baaf340a7eb5caa8a0b9e4e719b168d8eac69961
Daniel Byta <danielbyta.work@gmail.com>
2022-05-05 10:03:58 +0300

    fix puppeteer on linux machine

--- puppeteer/get_page_html.js
+++ puppeteer/get_page_html.js
@@ -2,7 +2,10 @@ import puppeteer from 'puppeteer'
 
 export async function getPageHtml (url) {
   try {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ['--no-sandbox']
+    })
     const [page] = await browser.pages()
 
     await page.goto(url, { waitUntil: 'networkidle0' });
